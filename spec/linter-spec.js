4fef284eccdc84a28ea77aff91a9e07cd53a93a3
ipaterson <ipaterson@newstex.com>
2017-02-28 19:12:10 -0500

    Use strict equality in test assertions where possible

--- spec/linter-spec.js
+++ spec/linter-spec.js
@@ -29,10 +29,10 @@ describe('Js-YAML provider for Linter', () => {
     waitsForPromise(() =>
       atom.workspace.open(badPath).then((editor) => {
         const messages = lint(editor);
-        expect(messages.length).toEqual(1);
-        expect(messages[0].type).toEqual('Error');
-        expect(messages[0].text).toEqual('end of the stream or a document separator is expected');
-        expect(messages[0].filePath).toMatch(/.+bad\.yaml$/);
+        expect(messages.length).toBe(1);
+        expect(messages[0].type).toBe('Error');
+        expect(messages[0].text).toBe('end of the stream or a document separator is expected');
+        expect(messages[0].filePath).toBe(badPath);
         expect(messages[0].range).toEqual([[2, 4], [2, 5]]);
       }),
     ),
@@ -42,7 +42,7 @@ describe('Js-YAML provider for Linter', () => {
     waitsForPromise(() =>
       atom.workspace.open(issue2Path).then((editor) => {
         const messages = lint(editor);
-        expect(messages.length).toEqual(0);
+        expect(messages.length).toBe(0);
       }),
     ),
   );
@@ -51,7 +51,7 @@ describe('Js-YAML provider for Linter', () => {
     waitsForPromise(() =>
       atom.workspace.open(issue9Path).then((editor) => {
         const messages = lint(editor);
-        expect(messages.length).toEqual(0);
+        expect(messages.length).toBe(0);
       }),
     ),
   );
